ae91d80557650a6bdbb446afca4f52e4be4cf285
LoicMonard <monard@intechinfo.fr>
2020-04-03 15:06:58 +0200

    Add navbar component
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="../assets/header.png"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
