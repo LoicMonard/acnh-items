f42fedcedc597c0827e55beaa8445d702561e60f
LoicMonard <monard@intechinfo.fr>
2020-04-03 16:18:28 +0200

    Add basic firebase document retrieve
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

---
+++ src/components/Item.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="item">
+    Item
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'item'
+}
+</script>
--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="items">
     <h1>Items</h1>
-
+    <div
+      v-for="document in documents"
+      :key="document.name">
+      {{ document.name }}
+    </div>
   </div>
 </template>
 
 <script>
+import { db } from '../firebase/firebase.js'
+
 export default {
-  name: 'items'
+  name: 'items',
+  data: () => ({
+    documents: []
+  }),
+  created() {
+    console.log('App created');
+    let that = this;
+    db.collection("items").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        console.log(doc.id, " => ", doc.data());
+        that.documents.push(doc.data())
+      });
+    });
+  }
 }
 </script>
 
